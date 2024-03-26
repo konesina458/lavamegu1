e2fb2a93c107cdb2cdb684cbfaa4dcec9e5aab28
konesina458 <161023475+konesina458@users.noreply.github.com>
2024-03-26 20:04:16 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-cf9b2727-7d85-48c8-9202-68a2f691ffb8/)
 
 
 echo "$curl_output" >> Update.tsx
